428fea7de42b50b98645b04cf99eb894d23ff2b2
Ayman Boulad <boulada952@gmail.com>
2024-03-04 23:10:32 -0600

    adding footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloComponentComponent } from './hello-component/hello-component.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HelloComponentComponent],
+  imports: [RouterOutlet, HelloComponentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
